cca7a8042d267c710e7be34ea7249053a8dbb7c7
Timo Sulg <timgluz@gmail.com>
2020-06-06 18:49:33 +0200

    cleanup routes; add basic tests;

--- src/tsp/route.rs
+++ src/tsp/route.rs
@@ -17,6 +17,10 @@ impl Route {
         Route { route }
     }
 
+    pub fn get(&self, pos: usize) -> Option<usize> {
+        self.route.get(pos).map(|r| r.clone())
+    }
+
     pub fn len(&self) -> usize {
         self.route.len()
     }
@@ -43,11 +47,30 @@ impl Route {
 }
 
 fn random_position_pair(n_items: usize) -> (usize, usize) {
+    let mut pair = random_pair(n_items);
+    let max_iter = 10;
+
+    for _ in 0..max_iter {
+        // make sure that indexes are different and not adjacent
+        if pair.1.wrapping_sub(pair.0) > 1 {
+            break;
+        }
+
+        pair = random_pair(n_items);
+    }
+
+    pair
+}
+
+fn random_pair(n_items: usize) -> (usize, usize) {
+    if n_items < 2 {
+        panic!("n_items must be bigger than 2");
+    }
+
     let mut rng = rand::thread_rng();
     let pos1 = rng.gen_range(0, n_items);
     let pos2 = rng.gen_range(0, n_items);
 
-    // TODO: make sure that abs_diff is bigger than 1
     if pos1 < pos2 {
         (pos1, pos2)
     } else {
@@ -66,5 +89,46 @@ fn swap_cities(route: &mut Vec<usize>, from: usize, to: usize) {
     // swap values from routes with reversed_seq
     for (i, swapped_val) in reversed_seq.iter().enumerate() {
         route[from + i] = swapped_val.clone();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tsp::kdtree;
+
+    #[test]
+    fn test_route_from_cities_with_1_elem() {
+        let cities = kdtree::build_points(&[vec![0.0, 0.0]]);
+        let route = Route::from_cities(&cities);
+
+        assert_eq!(1, route.len());
+        assert_eq!(Some(0), route.get(0));
+    }
+
+    #[test]
+    fn test_route_from_cities_with_2_elem() {
+        let cities = kdtree::build_points(&[vec![0.0, 0.0], vec![1.0, 1.0]]);
+        let route = Route::from_cities(&cities);
+
+        assert_eq!(2, route.len());
+        assert_eq!(Some(0), route.get(0));
+        assert_eq!(Some(1), route.get(1));
+    }
+
+    #[test]
+    fn test_route_random_successor_returns_new_route() {
+        let cities = kdtree::build_points(&[
+            vec![0.0, 0.0],
+            vec![0.0, 0.5],
+            vec![0.0, 1.0],
+            vec![1.0, 1.0],
+            vec![1.0, 0.0],
+        ]);
+
+        let route1 = Route::from_cities(&cities);
+        let route2 = route1.random_successor();
+
+        assert!(route1.len() == route2.len())
     }
 }
