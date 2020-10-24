08c760c09f332e73444f2ac58772d174486e5846
Timo Sulg <timgluz@gmail.com>
2020-10-24 12:57:09 +0200

    add visualization for nearest neighbor solver;

--- src/tsp/nearest_neighbor.rs
+++ src/tsp/nearest_neighbor.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 
 use super::kdtree::{self, KDPoint};
+use super::progress::{send_progress, ProgressMessage};
+use super::route::Route;
 use super::{Solution, SolverOptions};
 
 pub fn solve(cities: &[KDPoint], options: &SolverOptions) -> Solution {
@@ -8,15 +10,18 @@ pub fn solve(cities: &[KDPoint], options: &SolverOptions) -> Solution {
     let n_nearest = options.n_nearest;
 
     let cities_table: HashMap<usize, KDPoint> = cities.iter().map(|c| (c.id, c.clone())).collect();
-    let mut route: Vec<usize> = cities.iter().map(|c| c.id).collect();
+    let mut path: Vec<usize> = cities.iter().map(|c| c.id).collect();
 
+    send_progress(ProgressMessage::PathUpdate(Route::new(&path), 0.0));
     // run optimization round
-    for i in 0..(route.len() - 1) {
-        let id1 = route[i];
+    for i in 0..(path.len() - 1) {
+        let id1 = path[i];
         let city1 = cities_table[&id1].clone();
+        send_progress(ProgressMessage::CityChange(id1));
+
         let frontier = search_tree.nearest(&city1, n_nearest);
 
-        let id2 = route[i + 1];
+        let id2 = path[i + 1];
         let current_distance = city1.distance(&cities_table[&id2]);
 
         let search_result = frontier.nearest();
@@ -33,13 +38,15 @@ pub fn solve(cities: &[KDPoint], options: &SolverOptions) -> Solution {
 
         if next_distance < current_distance {
             let nearest_city_id = closest_item.point.id;
-            if let Some(nearest_pos) = route.iter().position(|&x| x == nearest_city_id) {
-                route.swap(i + 1, nearest_pos);
+            if let Some(nearest_pos) = path.iter().position(|&x| x == nearest_city_id) {
+                path.swap(i + 1, nearest_pos);
+
+                send_progress(ProgressMessage::PathUpdate(Route::new(&path), 0.0));
             }
         }
     }
 
-    let tour = Solution::new(&route, cities);
-
+    send_progress(ProgressMessage::Done);
+    let tour = Solution::new(&path, cities);
     tour
 }
