e87c53848ea657bea36d00885a8f519d2a811083
Timo Sulg <timgluz@gmail.com>
2020-10-24 13:03:38 +0200

    add visualizer for two_opt solver;

--- src/tsp/two_opt.rs
+++ src/tsp/two_opt.rs
@@ -1,26 +1,33 @@
 use super::kdtree::KDPoint;
+use super::progress::{send_progress, ProgressMessage};
+use super::route::Route;
 use super::{city_table_from_vec, Solution, SolverOptions};
 
 pub fn solve(cities: &[KDPoint], options: &SolverOptions) -> Solution {
     let n_indices = cities.len() - 1;
     let cities_table = city_table_from_vec(cities);
-    let mut route: Vec<usize> = cities.iter().map(|c| c.id).collect();
+    let mut path: Vec<usize> = cities.iter().map(|c| c.id).collect();
+
+    send_progress(ProgressMessage::PathUpdate(Route::new(&path), 0.0));
 
     let mut improved = true;
     while improved {
         improved = false;
         for i in 0..(n_indices - 2) {
-            for j in (i + 2)..n_indices {
-                let current_distance = cities_table[&route[i]]
-                    .distance(&cities_table[&route[i + 1]])
-                    + cities_table[&route[j]].distance(&cities_table[&route[j + 1]]);
+            send_progress(ProgressMessage::CityChange(path[i]));
 
-                let new_distance = cities_table[&route[i]].distance(&cities_table[&route[j]])
-                    + cities_table[&route[i + 1]].distance(&cities_table[&route[j + 1]]);
+            for j in (i + 2)..n_indices {
+                let current_distance = cities_table[&path[i]].distance(&cities_table[&path[i + 1]])
+                    + cities_table[&path[j]].distance(&cities_table[&path[j + 1]]);
+
+                let new_distance = cities_table[&path[i]].distance(&cities_table[&path[j]])
+                    + cities_table[&path[i + 1]].distance(&cities_table[&path[j + 1]]);
 
                 if new_distance < current_distance {
-                    swap_2opt(&mut route, i + 1, j);
+                    swap_2opt(&mut path, i + 1, j);
                     improved = true;
+
+                    send_progress(ProgressMessage::PathUpdate(Route::new(&path), new_distance));
 
                     if options.verbose {
                         println!(
@@ -33,18 +40,19 @@ pub fn solve(cities: &[KDPoint], options: &SolverOptions) -> Solution {
         }
     }
 
-    Solution::new(&route, cities)
+    send_progress(ProgressMessage::Done);
+    Solution::new(&path, cities)
 }
 
-fn swap_2opt(route: &mut Vec<usize>, from: usize, to: usize) {
+fn swap_2opt(path: &mut Vec<usize>, from: usize, to: usize) {
     if from >= to {
         return; // ignore if from to are equal or wrong order
     }
 
-    let reversed_seq: Vec<usize> = route[from..=to].iter().map(|x| x.clone()).rev().collect();
+    let reversed_seq: Vec<usize> = path[from..=to].iter().map(|x| x.clone()).rev().collect();
 
     for (i, swapped_val) in reversed_seq.iter().enumerate() {
-        route[from + i] = swapped_val.clone();
+        path[from + i] = swapped_val.clone();
     }
 }
 
@@ -55,20 +63,20 @@ mod tests {
 
     #[test]
     fn test_swap_2opt_2middle_elems_in_even_size_list() {
-        let mut route = vec![1, 2, 3, 4];
+        let mut path = vec![1, 2, 3, 4];
 
-        swap_2opt(&mut route, 1, 2);
+        swap_2opt(&mut path, 1, 2);
 
-        assert_eq!(vec![1, 3, 2, 4], route);
+        assert_eq!(vec![1, 3, 2, 4], path);
     }
 
     #[test]
     fn test_swap_2opt_single_middle_elem_does_nothing() {
-        let mut route = vec![1, 2, 3, 4];
+        let mut path = vec![1, 2, 3, 4];
 
-        swap_2opt(&mut route, 1, 1);
+        swap_2opt(&mut path, 1, 1);
 
-        assert_eq!(vec![1, 2, 3, 4], route);
+        assert_eq!(vec![1, 2, 3, 4], path);
     }
 
     #[test]
@@ -84,6 +92,6 @@ mod tests {
         let default_opts = SolverOptions::default();
         let tour = solve(&cities, &default_opts);
         assert_eq!(4.0, tour.total);
-        assert_eq!(&[0, 1, 2, 3, 4], tour.route());
+        assert_eq!(&[0, 1, 2, 3, 4], tour.path());
     }
 }
