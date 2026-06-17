2bfe0692bb19bc50740d12e78d1a240d7b0a0c74
Timo Sulg <timgluz@gmail.com>
2026-06-17 20:58:25 +0200

    add missing updates

--- teeline-web/src/sentry.js
+++
@@ -1,9 +0,0 @@
-import * as Sentry from "@sentry/browser";
-
-Sentry.init({
-  dsn: "https://ec76cc3371026e1e4feb8ececea14aee@o4511523471228928.ingest.de.sentry.io/4511523482107984",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true,
-  tunnel: "/tunnel",
-});
